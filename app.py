3ce6c7fd416b38a39a75918ffd7cb2ba660c8cb1
Ace Figueroa <ace.g.fig@gmail.com>
2021-04-02 08:21:44 -0700

    Added server ticks

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from threading import Lock
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 import json
@@ -6,31 +7,40 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+# Threading stuff
+thread = None
+thread_lock = Lock()
+
+# Player data
 players = {}
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@socketio.on('connect')
-def onConnect():
-    print("USER CONNECTED")
-    print(request.sid)
-    players[request.sid] = [0, 0]
-    socketio.emit("update", json.dumps(players))
-
-@socketio.on('disconnect')
-def onDisconnect():
-    print("USER DISCONNECTED")
-    print(request.sid)
-    del players[request.sid]
-    print(players)
-    socketio.emit("update", json.dumps(players))
-
 @socketio.on("update")
 def update(x, y):
     players[request.sid] = [x, y]
-    socketio.emit("update", json.dumps(players))
+
+def server_tick():
+    while True:
+        socketio.sleep(1/60)
+        socketio.emit('server event', json.dumps(players))
+
+@socketio.event
+def connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(server_tick)
+
+    print(f"USER[{request.sid}] CONNECTED")
+    players[request.sid] = [0, 0]
+
+@socketio.event
+def disconnect():
+    print(f"USER[{request.sid}] DISCONNECTED")
+    del players[request.sid]
 
 if __name__ == '__main__':
     socketio.run(app)
